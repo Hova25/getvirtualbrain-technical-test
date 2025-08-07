852695dc89ce8a3e2fae0025dba38b02cb1940f9
Hovannes <hovannes.markarian@gmail.com>
2025-08-07 13:33:20 +0400

    feat(change-ui): fix update search params to keep state

--- apps/web-app/src/features/pokemon-list/filters/FilterInput.tsx
+++ apps/web-app/src/features/pokemon-list/filters/FilterInput.tsx
@@ -1,5 +1,5 @@
 import {ChangeEvent} from "react";
-import {useSearchParams} from "react-router-dom";
+import {useLocation, useNavigate, useSearchParams} from "react-router-dom";
 
 import {Input} from "../../../components/ui/Input.tsx";
 import {useDebounceCallback} from "../../../hooks/useDebounceCallback.ts";
@@ -7,15 +7,29 @@ import {useDebounceCallback} from "../../../hooks/useDebounceCallback.ts";
 export const POKEMON_LIST_SEARCH_PARAM_SEARCH = "search";
 
 export const FilterInput = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
 
   const debouncedSearch = useDebounceCallback((value: string) => {
+    const newParams = new URLSearchParams(searchParams);
+
     if (value) {
-      searchParams.set(POKEMON_LIST_SEARCH_PARAM_SEARCH, value);
+      newParams.set(POKEMON_LIST_SEARCH_PARAM_SEARCH, value);
     } else {
-      searchParams.delete(POKEMON_LIST_SEARCH_PARAM_SEARCH);
+      newParams.delete(POKEMON_LIST_SEARCH_PARAM_SEARCH);
     }
-    setSearchParams(searchParams, { replace: true })
+
+    navigate(
+      {
+        pathname: location.pathname,
+        search: newParams.toString(),
+      },
+      {
+        replace: true,
+        state: location.state,
+      }
+    );
   }, 200);
 
   const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
--- apps/web-app/src/features/pokemon-list/filters/PokemonListFilters.tsx
+++ apps/web-app/src/features/pokemon-list/filters/PokemonListFilters.tsx
@@ -9,7 +9,7 @@ import {PokemonTypesSelect} from "./PokemonTypesSelect";
 export const PokemonListFilters = () => {
   const {state} = useLocation();
   const {step} = (state || {}) as PokemonListPageState;
-  return <Card className="py-4 flex flex-col gap-4 items-center justify-center">
+  return <Card className="py-4 flex flex-col gap-4 items-center justify-center z-40">
     <span>Selection du {step === "POKEMON_1"? "premier" : "second"} Pokemon !</span>
     <div className="flex flex-col md:flex-row  gap-4">
       <FilterInput />
